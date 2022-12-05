01162c34e1a4ad16a1acf1b721970e6091984663
Raphael <raphaelheineck@gmail.com>
2022-12-05 11:17:29 -0300

    added auth route and package axios

--- web/src/pages/SignUp/index.jsx
+++ web/src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+
 import { Container, Form, Background } from "./styles";
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from "../../services/api"
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function SignUp() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSignUp () {
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!")
+        }
+
+        api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário Cadastrado com sucesso!")
+            navigate("/")
+        })
+        .catch(error => {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi posspivel cadastrar!")
+            }
+        })
+    }
+
     return(
         <Container>
             <Background />
@@ -19,6 +48,7 @@ export function SignUp() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={event => setName(event.target.value)}
                 />
 
 
@@ -26,15 +56,17 @@ export function SignUp() {
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignUp}/>
 
                 <Link to="/">Voltar para o Login</Link>
 
